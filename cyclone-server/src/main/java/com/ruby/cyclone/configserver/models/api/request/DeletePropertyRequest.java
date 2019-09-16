143ff04cf411868a4ea7e72a724534cd2a65c4da
Catalin.Cimpoeru <catalin.cimpoeru@orange.com>
2019-09-16 07:19:19 +0200

    create dummy controller #4

--- cyclone-server/src/main/java/com/ruby/cyclone/configserver/models/api/request/DeletePropertyRequest.java
+++ cyclone-server/src/main/java/com/ruby/cyclone/configserver/models/api/request/DeletePropertyRequest.java
@@ -10,5 +10,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class DeletePropertyRequest {
-    private String propertyRequest;
+    private String propertyKey;
 }
